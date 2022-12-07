10e3e6510be50cdedefba24278cc74c163b70614
thompson <alfredthompsonovie@gmail.com>
2022-12-08 00:56:47 +0100

    home page desktop view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,16 +6,18 @@
       </RouterLink>
     </header>
 
-    <section>
-      <div class="hero--mobile">
+    <section class="hero__wrapper">
+      <div class="hero">
         <img 
         src="@/assets/images/HeroImage-Mobile.jpg" 
         alt="an image of people camping"
+        class="hero__img--mobile"
         v-if="mobile"
         >
         <img 
-        src="@/assets/images/HeroImage-Mobile.jpg" 
+        src="@/assets/images/HeroImage.jpg" 
         alt="an image of people camping"
+        class="hero__img--desktop"
         v-else
         >
 
@@ -24,43 +26,44 @@
     
 
 
-
-    <section class="home-content">
-      <h1>
-        Explore the best camps on Earth.
-      </h1>
-      <p>
-        YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.
-      </p>
-      <ul class="feedback--list">
-        <li class="feedback--item">
-          <img src="@/assets/images/Checkmark.svg" alt="Checkmark">
-          <span>Add your own camp suggestions.</span>
-        </li>
-        <li class="feedback--item">
-          <img src="@/assets/images/Checkmark.svg" alt="Checkmark">
-          <span>Leave reviews and experiences.</span>
-        </li>
-        <li class="feedback--item">
-          <img src="@/assets/images/Checkmark.svg" alt="Checkmark">
-          <span>See locations for all camps.</span>
-        </li>
-      </ul>
-      <RouterLink to="/about" class="cta">View Campgrounds</RouterLink>
-    </section>
-    <section class="sponsors">
-      <p>Partnered with:</p>
-      <ul>
-        <li>
-          <img src="@/assets/images/Airbnb.svg" alt="airbnb logo">
-        </li>
-        <li>
-          <img src="@/assets/images/Booking.svg" alt="booking.com logo">
-        </li>
-        <li>
-          <img src="@/assets/images/PlumGuide.svg" alt="plum Guide logo">
-        </li>
-      </ul>
+    <section class="content__wrapper">
+      <section class="home-content">
+        <h1>
+          Explore the best camps on Earth.
+        </h1>
+        <p>
+          YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.
+        </p>
+        <ul class="feedback--list">
+          <li class="feedback--item">
+            <img src="@/assets/images/Checkmark.svg" alt="Checkmark">
+            <span>Add your own camp suggestions.</span>
+          </li>
+          <li class="feedback--item">
+            <img src="@/assets/images/Checkmark.svg" alt="Checkmark">
+            <span>Leave reviews and experiences.</span>
+          </li>
+          <li class="feedback--item">
+            <img src="@/assets/images/Checkmark.svg" alt="Checkmark">
+            <span>See locations for all camps.</span>
+          </li>
+        </ul>
+        <RouterLink to="/about" class="cta">View Campgrounds</RouterLink>
+      </section>
+      <section class="sponsors">
+        <p>Partnered with:</p>
+        <ul>
+          <li>
+            <img src="@/assets/images/Airbnb.svg" alt="airbnb logo">
+          </li>
+          <li>
+            <img src="@/assets/images/Booking.svg" alt="booking.com logo">
+          </li>
+          <li>
+            <img src="@/assets/images/PlumGuide.svg" alt="plum Guide logo">
+          </li>
+        </ul>
+      </section>
     </section>
   </main>
 </template>
@@ -70,14 +73,14 @@ import { ref, onMounted } from 'vue';
 
 export default {
   components: {
-    // BaseInput
+    
   },
   setup() {
     const mobile = ref(null);
     const windowWidth = ref(null);
     function checkScreen() {
       windowWidth.value = window.innerWidth;
-      if (windowWidth.value > 768) {
+      if (windowWidth.value > 991) {
         mobile.value = false
       } else {
         mobile.value = true
@@ -100,15 +103,29 @@ export default {
     padding: 1.5em 1em;
   }
 
+  .hero__img--mobile {
+    min-height: 15em;
+    object-fit: cover;
+  }
+  .hero {
+    height: 100%;
+  }
+  .hero__img--desktop {
+    height: inherit;
+    width: 100%;
+  }
   h1 {
     font-size: 2rem;
     line-height: 1;
   }
-  .home-content, .sponsors {
-    padding: 1em;
+  .home-content {
+    padding: 1.5em 1.2em 0;
   }
   .home-content > * {
     margin-bottom: .5em;
+  }
+  .feedback--list {
+    margin-top: .8em;
   }
   .feedback--item {
     display: flex;
@@ -126,9 +143,34 @@ export default {
     color: #fff;
     margin-top: 1em;
   }
+  .sponsors {
+    padding: 1em 1.2em;
+  }
   .sponsors ul {
     display: flex;
     align-items: center;
 
   }
+
+  @media (min-width: 992px) {
+    main {
+      display: grid;
+      grid-template-rows: 6em calc(100vh - 6em);
+      grid-template-columns: .2fr 1.5fr 1fr;
+      /* align-items: center; */
+    }
+    header {
+      grid-row: 1;
+      grid-column: 2;
+    }
+    .hero__wrapper {
+      grid-column: 3;
+      grid-row: 1/-1;
+    }
+    .content__wrapper {
+      grid-row: 2;
+      grid-column: 2;
+      align-self: center;
+    }
+  } 
 </style>
