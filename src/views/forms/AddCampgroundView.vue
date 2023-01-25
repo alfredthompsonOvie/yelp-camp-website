cdb7a609cee2845182ddba3c08d8b5926af8ef0f
thompson <alfredthompsonovie@gmail.com>
2023-01-25 08:57:19 +0100

    validated file input

--- src/views/forms/AddCampgroundView.vue
+++ src/views/forms/AddCampgroundView.vue
@@ -8,7 +8,7 @@
 			<form class="form" @submit.prevent="submit">
 				<fieldset class="form-contents form__contents">
 					<legend class="form__title">Add New Campground</legend>
-				
+
 					<BaseInput
 						label="Campground Name"
 						placeholder="Seven Sisters Waterfall"
@@ -28,18 +28,32 @@
 						v-model="price"
 						:error="errors.price"
 					/>
-
-					<BaseInput
+					<!--! IMAGE -->
+					<!-- <BaseInput
 						label="Image"
 						placeholder="www.thepinoytravelter.com/2018/01/mt-ulap-diy-dayhike.html"
 						className="input--addCamp"
-						type="text"
+						type="file"
 						name="image"
-						v-model="image"
 						:error="errors.image"
-					/>
+						v-model="image"
+						/> -->
+					<div class="form__control">
+						<label for="image">image: </label>
+						<input
+							className="input--addCamp"
+							type="file"
+							id="image"
+							name="image"
+							@change="handleChange($event)"
+						/>
+						<!-- accept="image/png, image/jpeg" -->
 
-					<BaseTextArea 
+						<p>{{ fileError }}</p>
+						<p>{{ errors.image }}</p>
+					</div>
+
+					<BaseTextArea
 						label="Description"
 						name="description"
 						placeholder="The Seven Sisters is the 39th tallest waterfall in Norway. The 410-meter tall waterfall consists of seven separate streams, and the tallest of the seven has a free fall that measures 250 metres. The waterfall is located along the Geirangerfjordan in Stranda Municipality in More og Romsdal county. Norway."
@@ -67,48 +81,84 @@ import BaseInput from "@/components/BaseInput.vue";
 import BaseTextArea from "@/components/BaseTextArea.vue";
 
 import { useField, useForm } from "vee-validate";
-import { object, string, number } from "yup";
+import { object, string, number, mixed } from "yup";
 
-import { useRouter } from "vue-router"
+import { useRouter } from "vue-router";
+import { ref } from "vue";
 
 export default {
 	name: "AddCampgroundView",
 	components: {
 		MainNav,
 		BaseInput,
-		BaseTextArea
+		BaseTextArea,
 	},
 	setup() {
 		const router = useRouter();
+		const fileError = ref(null);
+		const AllowedFileTypes = ref(
+			[
+				"image/png",
+				"image/jpg",
+				"image/jpeg",
+				"image/svg",
+				"image/webp",
+			]);
+
+		// function isValidFileType(fileName, fileType) {
+		// 	return (
+		// 		fileName &&
+		// 		validFileExtensions[fileType].indexOf(fileName.split(".").pop()) > -1
+		// 	);
+		// }
+		function isValidFileType(fileType) {
+			console.log(fileType);
+			return  AllowedFileTypes.value.includes(fileType)
+		}
+
 		// validationSchema
-		const schema = object({
-			campgroundName: string().required(),
-			price: number().required(),
-			image: string().required(),
-			description: string().required(),
-		})
+		const schema = object().shape({
+			campgroundName: string().required("This field is required"),
+			price: number().required("This field is required"),
+
+			image: mixed()
+			.required("This field is required")
+				.test(
+					"isValidType",
+					"Please provide a valid image type (.png, .jpg, .jpeg, .svg, .webp)",
+					value => isValidFileType(value?.type),
+				),
+			description: string().required("This field is required"),
+		});
 
 		const { handleSubmit, errors } = useForm({
-			validationSchema: schema
-		})
+			validationSchema: schema,
+		});
 
-		const { value: campgroundName } = useField('campgroundName')
-		const { value: price } = useField('price')
-		const { value: image } = useField('image')
-		const { value: description } = useField('description')
+		const { value: campgroundName } = useField("campgroundName");
+		const { value: price } = useField("price");
+		const { handleChange, value: image } = useField("image");
+		const { value: description } = useField("description");
 
-		const submit = handleSubmit(values => {
+		const submit = handleSubmit((values) => {
 			console.log("submited", values);
-			router.push({ name: "campgrounds" })
+			// router.push({ name: "campgrounds" })
+		});
 
-		})
+		handleChange(($event) => {
+			console.log($event);
+		});
+
 		return {
 			submit,
 			campgroundName,
 			price,
 			image,
+			handleChange,
 			description,
-			errors
+			errors,
+			// value,
+			fileError,
 		};
 	},
 };
@@ -138,5 +188,5 @@ main {
 		padding-inline: 0;
 		grid-column: 2/5;
 	}
-	}
+}
 </style>
